389237080afe34c5e460943a2c6bb87062725dcb
Shiwu <49446319+shichuyibushishiwu@users.noreply.github.com>
2025-12-12 14:58:07 +0800

    add application name

--- template/content/Tuna.Revit.Template/App.cs
+++ template/content/Tuna.Revit.Template/App.cs
@@ -1,20 +1,22 @@
 ﻿using Autodesk.Revit.UI;
 
 /// Power by shiwu
-namespace Tuna.Revit.Template;
-
-/// <summary>
-/// The revit application plugin
-/// </summary>
-public class App : IExternalApplication
+namespace Tuna.Revit.Template
 {
-    public Result OnShutdown(UIControlledApplication application)
+    /// <summary>
+    /// The revit application plugin
+    /// </summary>
+    public class App : IExternalApplication
     {
-        throw new NotImplementedException();
-    }
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            throw new NotImplementedException();
+        }
 
-    public Result OnStartup(UIControlledApplication application)
-    {
-        throw new NotImplementedException();
+        public Result OnStartup(UIControlledApplication application)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
+
